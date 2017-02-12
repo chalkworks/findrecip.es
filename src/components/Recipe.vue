c04b803be7616b3e13dce783ee4175681dff1687
Kabir Shah <simplecooldude1@gmail.com>
2017-02-12 13:46:29 -0800

    add hover effect on cards

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -20,6 +20,9 @@
   background light-background
   box-shadow 0px 10px 50px header-color
   border-radius 10px
+  transition all 0.2s ease
+  &:hover
+    box-shadow 0px 5px 75px header-color
   h3
     color header-color
     font-weight 700
